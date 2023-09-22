b2468fbedaf1cd0844a69ac4d8df8e92e8517114
mouhamedamine <mouhamedamine.akchouche@gmail.com>
2023-09-22 12:16:40 -0700

    fixing_bugs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,50 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t compteur= 0;
+    size_t compteur = 0;
 
-	while (h)
-	{
-		if (h->str)
-			_putchar(h->str);
-			
-		else
-			_putchar("[0] (nil)\n");
-		h = h->next;
-		compteur++;
-	}
-	return (compteur);
+    while (h)
+    {
+        if (h->str)
+        {
+            unsigned int len = h->len;
+            while (len > 0)
+            {
+                _putchar('0' + len % 10);
+                len /= 10;
+            }
+            _putchar(']');
+            _putchar(' ');
+            for (unsigned int i= 0; i < h->len; i++)
+            {
+                _putchar(h->str[i]);
+            }
+            _putchar('\n');
+        }
+        else
+        {
+            _putchar('[');
+            _putchar('0');
+            _putchar(']');
+            _putchar(' ');
+            _putchar('(');
+            _putchar('n');
+            _putchar('i');
+            _putchar('l');
+            _putchar(')');
+            _putchar('\n');
+        }
+        h = h->next;
+        compteur++;
+    }
+
+    return (compteur);
 }
