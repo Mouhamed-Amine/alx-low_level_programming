6f12c6bbe9cc5d040762f37c2d4f614740c24e1c
mouhamedamine <mouhamedamine.akchouche@gmail.com>
2023-09-22 14:06:11 -0700

    last_bug_fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 
 /**
- * print_list - prints a list_t linked list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * @h: first node of linked list to print
- *
- * Return: number of nodes printed
+ * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t len = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		len++;
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (len);
+
+	return (nodes);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the list
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: the number of nodes
+ * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
@@ -12,8 +13,9 @@ size_t list_len(const list_t *h)
 
 	while (h)
 	{
-		h = h->next;
 		compteur++;
+		h = h->next;
 	}
+
 	return (compteur);
 }
